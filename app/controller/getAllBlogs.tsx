ab398eaec7f8ff320197007f45a1a19403e3fcda
vasu <vasumitragajbhiye20@gmail.com>
2025-10-12 16:58:27 +0530

    hope it works

--- app/controller/getAllBlogs.tsx
+++ app/controller/getAllBlogs.tsx
@@ -2,7 +2,7 @@ export default async function getAllSubjects() {
   try {
     const res = await fetch(`https://alevel-testing.netlify.app/api/blogs`, {
       // const res = await fetch(`http://localhost:3000/api/blogs`, {
-      // mode: "no-cors",
+      // mode: "no-cors", 
     });
     const sub = await res.json();
     return sub.data;
