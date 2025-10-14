f626f998f1e32061e94bc86e2321262e8ffca4a5
vasu <vasumitragajbhiye20@gmail.com>
2025-10-14 19:58:29 +0530

    new

--- app/controller/getAllTeam.tsx
+++ app/controller/getAllTeam.tsx
@@ -2,7 +2,7 @@ export default async function getAllSubjects() {
   try {
      const apiLink =process.env.NEXT_PUBLIC_GETALLTEAM;
         const res = await fetch(`${apiLink}`);
-    // const res = await fetch(`https://alevel-testing.netlify.app/api/team`);
+    // const res = await fetch(`https://alevel-testigit ng.netlify.app/api/team`);
     // const res = await fetch(`http://localhost:3000/api/team`);
     const sub = await res.json();
     return sub.data;
