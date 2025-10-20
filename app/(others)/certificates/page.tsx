090501322313b73458ca90dfdb92993f6f7323bc
unknown <jakeschwegler@gmail.com>
2025-10-20 22:13:56 +0700

    Added certificate 2a00rt412uji

--- app/(others)/certificates/page.tsx
+++ app/(others)/certificates/page.tsx
@@ -9,7 +9,8 @@ const Search = function () {
     { name: "2g00of311lek", id: "2g00of311lek" },
     { name: "2d00io308jke", id: "2d00io308jke" },
     { name: "2h00vd310gdf", id: "2h00vd310gdf" },
-    {name: "2t00ml394ndi", id: "2t00ml394ndi"},
+    { name: "2t00ml394ndi", id: "2t00ml394ndi" },
+    { name: "2a00rt412uji", id: "2a00rt412uji" },
   ];
 
   const items = [
@@ -18,6 +19,7 @@ const Search = function () {
     { name: "2d00io308jke", id: 3 },
     { name: "2h00vd310gdf", id: 4 },
     { name: "2t00ml394ndi", id: 5 },
+    { name: "2a00rt412uji", id: 6 },
   ];
 
   const handleOnSearch = (string: any, results: any) => {
